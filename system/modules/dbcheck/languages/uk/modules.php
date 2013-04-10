8b143f209ebb1efc61a3ba262636ad2738f1ef73
Sergey Dyagovchenko <sergey.dyagovchenko@gmail.com>
2013-04-10 11:38:47 +0300

    Add ukrainian language

---
+++ system/modules/dbcheck/languages/uk/modules.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * @package   DBCheck
+ * @author    Sergey Dyagovchenko aka DyaGa
+ * @license   GNU/LGPL
+ * @copyright Sergey Dyagovchenko 2012-2013
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['dbcheck'] = array('Перевірка бази даних', 'Перевірка/оновлення бази даних на наявність змін в таблицях');
